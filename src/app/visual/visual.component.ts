025d0d70df729afc2909f73a123650a990da81ee
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-05-03 09:23:27 +0500

    add load screen

--- src/app/visual/visual.component.ts
+++ src/app/visual/visual.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visual.component.scss']
 })
 export class VisualComponent implements OnInit {
+  condit: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {this.condit = false} , 2000);
   }
 
 }
